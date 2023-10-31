58ef026da9a32bb175addf9be81ca814ae07693c
AnasAshour <53153793+anas0015@users.noreply.github.com>
2023-10-31 08:37:19 +0000

    finished the FirmaForm

--- nextjs-bav/src/app/components/form/FirmaForm.tsx
+++ nextjs-bav/src/app/components/form/FirmaForm.tsx
@@ -8,13 +8,26 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
 const FormSchema = z.object({
-    username: z.string().min(2, {
+    firmaName: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    firmaStrasse: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    firmaHausnummer: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    firmaPlz: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    firmaOrt: z.string().min(2, {
       message: "Username must be at least 2 characters.",
     }),
   })
 
-const FirmaForm = () => {
 
+
+const FirmaForm = () => {
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
     });
@@ -23,83 +36,95 @@ const FirmaForm = () => {
 
 
     return (
-        
-        <FormProvider {...form}>
-        <form className="w-2/3 space-y-6">
+    <div className="p-6 bg-white
+    border w-11/12 h-max border-gray-200 rounded-lg shadow-2xl">    
+      <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+      <p className=" mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Firma</p>
+      </div>
+      <FormProvider {...form}>
+        <form className="w-full space-y-6">
           <FormField
             control={form.control}
-            name="username"
+            name="firmaName"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Firma</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input className="w-full" placeholder="CodeCamp:N" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
 
-<FormField
+        
+        <div className="grid grid-cols-2 gap-2">
+
+          <FormField
             control={form.control}
-            name="username"
+            name="firmaStrasse"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Straße</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input placeholder="Musterstraße" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
 
-<FormField
+          <FormField
             control={form.control}
-            name="username"
+            name="firmaHausnummer"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Hausnummer</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input placeholder="1" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
 
-<FormField
+        </div>
+
+        <div className="grid grid-cols-2 gap-2">
+
+          <FormField
             control={form.control}
-            name="username"
+            name="firmaPlz"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>PLZ</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input placeholder="90518" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
 
-<FormField
+          <FormField
             control={form.control}
-            name="username"
+            name="firmaOrt"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Ort</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input placeholder="Musterort" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-
+        </div>
           <Button type="submit">Submit</Button>
         </form>
       </FormProvider>
-    )
+    </div>
+  )
 };
 
 export default FirmaForm
--- nextjs-bav/src/app/vertrag/page.tsx
+++ nextjs-bav/src/app/vertrag/page.tsx
@@ -2,9 +2,10 @@ import FirmaForm from "../components/form/FirmaForm";
 
 const page = () => {
     return ( 
-        <div className="p-6 bg-white border w-11/12 h-max border-gray-200 rounded-lg shadow-2xl "> 
-        <p className=" mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">Neurer Antrag</p>
-            <FirmaForm /> 
+        <div className="p-6 bg-white
+         border w-11/12 h-max border-gray-200 rounded-lg shadow-2xl "> 
+            <p className=" mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">Neurer Antrag</p>
+                <FirmaForm /> 
         </div>
     )
 };
