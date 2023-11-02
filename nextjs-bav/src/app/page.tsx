f95bfd7e10967f89b474b7def0c0f8f51b5a239c
AnasAshour <53153793+anas0015@users.noreply.github.com>
2023-11-02 10:16:31 +0000

    added accordion

--- nextjs-bav/src/app/page.tsx
+++ nextjs-bav/src/app/page.tsx
@@ -1,15 +1,22 @@
 
 import Link from "next/link";
-import { buttonVariants } from "./components/ui/button";
+import { Button } from "./components/ui/button";
 
 
 export default function Home() {
   return (
-    <>
-    <h1 className="mb-8 text-6xl">Betriebliche Altersvorsorge</h1>
-      <Link className={buttonVariants({size:"lg"})} href="/vertrag">
-        <div className="text-2xl mb-3">Neuer Antrag</div>
-      </Link>
-    </>
-     )
+
+    <div className="flex flex-col justify-center">
+      <h1 className="mt-8 mb-8 text-6xl">Betriebliche Altersvorsorge</h1>
+      <div className="flex justify-center">
+
+        <Button asChild>
+          <Link href="/vertrag">      
+            Neuer Vertrag
+          </Link>
+        </Button>
+      </div>
+    </div>
+
+  )
 }
